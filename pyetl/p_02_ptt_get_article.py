68534977f4693db0b294ba185bd07ba94b354063
uuboyscy <aegis12321@gmail.com>
2024-11-09 11:45:36 +0800

    ptt article

---
+++ pyetl/p_02_ptt_get_article.py
@@ -0,0 +1,26 @@
+import requests
+from bs4 import BeautifulSoup
+
+url = "https://ptt-discussion.tw/bbs/Gossiping/index.html"
+headers = {
+    "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/130.0.0.0 Safari/537.36",
+}
+cookies = {
+    "over18": "1",
+}
+
+response = requests.get(url, headers=headers, cookies=cookies)
+soup = BeautifulSoup(response.text, "html.parser")
+
+articles = soup.find_all("div", class_="title")
+print(articles)
+for article in articles:
+    # Each article be like:
+    # <div class="title">
+    # <a href="/bbs/Gossiping/M.1731081383.A.48A.html">[新聞] 川普勝選太傷心？哈佛大學隔天取消課程</a>
+    # </div>
+    title = article.text.strip()  # article.find("a").text.strip()
+    link = article.find("a")  # <a href="/bbs/Gossiping/M.1731081383.A.48A.html">[新聞] 川普勝選太傷心？哈佛大學隔天取消課程</a>
+    if link:
+        url = "https://ptt-discussion.tw" + link["href"]
+        print(f"Title: {title}, URL: {url}")
