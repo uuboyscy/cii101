b8937bd849f0a5a116ef28b5deb7f407625419ea
uuboyscy <aegis12321@gmail.com>
2024-11-09 13:08:51 +0800

    ptt article

--- pyetl/p_02_ptt_get_article_content.py
+++ pyetl/p_02_ptt_get_article_content.py
@@ -10,7 +10,8 @@ PTT_FOLDER_PATH = "ptt_article"
 if not os.path.exists(PTT_FOLDER_PATH):
     os.mkdir(PTT_FOLDER_PATH)
 
-url = "https://ptt-discussion.tw/bbs/Gossiping/index.html"
+# url = "https://ptt-discussion.tw/bbs/Gossiping/index.html"
+url = "https://www.ptt.cc/bbs/Gossiping/index.html"
 headers = {
     "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/130.0.0.0 Safari/537.36",
 }
@@ -31,5 +32,7 @@ for article in articles:
     title = article.text.strip()  # article.find("a").text.strip()
     link = article.find("a")  # <a href="/bbs/Gossiping/M.1731081383.A.48A.html">[新聞] 川普勝選太傷心？哈佛大學隔天取消課程</a>
     if link:
-        url = "https://ptt-discussion.tw" + link["href"]
-        print(f"Title: {title}, URL: {url}")
+        article_url = "https://www.ptt.cc" + link["href"]
+        print(f"Title: {title}, URL: {article_url}")
+
+        # Get article content "string"
